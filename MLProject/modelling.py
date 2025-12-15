476b30ad91546b226cc2d53296a13621a4f55f79
Leonardo Sanjaya <leonardosanjaya23@gmail.com>
2025-12-15 23:55:24 +0700

    Menambahkan seluruh file yang dibutuhkan

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,13 +5,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import mlflow
+import sys
+
+learning_rate = sys.argv[1] 
+max_depth = sys.argv[2]
+n_estimators = sys.argv[3]
+dataset_path = sys.argv[4]
 
 # Setup MLflow
 mlflow.set_tracking_uri("http://127.0.0.1:5000/")
 mlflow.set_experiment("Submission MSML - Car Price Regression")
 
 # Load Dataset
-df = pd.read_csv('used_car_price_dataset_extended_preprocessing.csv')
+df = pd.read_csv(dataset_path)
 
 # Split Dataset
 X = df.drop('price_usd', axis=1)
@@ -21,16 +27,15 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 input_example = X_train[0:5]
 
 with mlflow.start_run(run_name="GBR_standard"):
-    # Parameter yang dicoba & log
     loss='squared_error'
-    learning_rate=0.1
-    max_depth=10
     random_state=42
     mlflow.autolog()
 
-    # Train model
+    # Train model menggunakan parameter yang dimasukkan 
+    # langsung secara otomatis melalui file MLProject (Workflow-CI)
     model = GradientBoostingRegressor(loss=loss, learning_rate=learning_rate, 
-                                      max_depth=max_depth, random_state=random_state)
+                                      max_depth=max_depth, random_state=random_state, 
+                                      n_estimators=n_estimators)
     mlflow.sklearn.log_model(
         sk_model=model,
         artifact_path="model",
